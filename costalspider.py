c662cf9ccdeccb4289d4780370b169a694801646
cheunyu <cheunyufoo@gmail.com>
2018-02-02 18:41:43 +0800

    代码模块化 参考http://blog.csdn.net/bo_wen_/article/details/50868339

--- costalspider.py
+++ costalspider.py
@@ -6,45 +6,55 @@ import pymysql
 class CostalSpider(object):
     """docstring for Spider"""
 
-    def __init__(self, ocean_name, forecast_time, phenomenon, wind_direction, wind_power, visibility):
-        super(Spider, self).__init__()
-        self.ocean_name = ocean_name
-        self.forecast_time = forecast_time
-        self.phenomenon = phenomenon
-        self.wind_direction = wind_direction
-        self.wind_power = wind_power
-        self.visibility = visibility
+    # def __init__(self, ocean_name, forecast_time, phenomenon, wind_direction, wind_power, visibility):
+    #     super(Spider, self).__init__()
+    #     self.ocean_name = ocean_name
+    #     self.forecast_time = forecast_time
+    #     self.phenomenon = phenomenon
+    #     self.wind_direction = wind_direction
+    #     self.wind_power = wind_power
+    #     self.visibility = visibility
 
+    def get_content(url):
+        response = urllib.request.urlopen(url)
+        return response.read().decode('utf-8')
 
-response = urllib.request.urlopen(
-    'http://www.nmc.cn/publish/marine/newcoastal.html')
-html = response.read().decode('utf-8')
-soup = BeautifulSoup(html, "lxml")
-marineArea = soup.select('td[rowspan=6]')
-# for i in marineArea:
-#     print(i.string)
-tr = soup.select('tr[align=left]')
-db = pymysql.connect("127.0.0.1", "marine_weather",
-                     "qwe123", "marine_weather", charset="utf8")
-cursor = db.cursor()
-cursor.execute("SELECT VERSION()")
-print(cursor.fetchone())
-for i in tr:
-    ocean_name = i.attrs['name']
-    data_arr = []
-    td = i.select('td')
-    for j in range(len(td)):
-        if(len(td) == 6):
-            if(j != 5):
-                # print(td[j + 1])
-                data_arr.append(td[j + 1])
-        else:
-            data_arr.append(td[j])
-    try:
-        cursor.execute('INSERT INTO COASTAL(OCEAN_NAME,FORECAST_TIME,PHENOMENON,WIND_DIRECTION,WIND_POWER,VISIBILITY) VALUES ("%s","%s","%s","%s","%s","%s")' %
-                       (ocean_name, data_arr[0].string, data_arr[1].string, data_arr[2].string, data_arr[3].string, data_arr[4].string))
-        db.commit()
-    except Exception as e:
-        print(e)
-        db.rollback()
-db.close()
+    def get_data(html_text):
+        soup = BeautifulSoup(html_text, "lxml")
+        marineArea = soup.select('td[rowspan=6]')
+        tr = soup.select('tr[align=left]')
+        final = []
+        for i in tr:
+            ocean_name = i.attrs['name']
+            weatherdata_arr = [ocean_name]
+            td = i.select('td')
+            for j in range(len(td)):
+                if(len(td) == 6):
+                    if(j != 5):
+                        weatherdata_arr.append(td[j + 1].string)
+                else:
+                    weatherdata_arr.append(td[j].string)
+            final.append(weatherdata_arr)
+            
+        return final
+
+    def save_db(data_arr):
+
+        db = pymysql.connect("127.0.0.1", "marine_weather",
+                             "qwe123", "marine_weather", charset="utf8")
+        cursor = db.cursor()
+        for i in data_arr:
+            try:
+                cursor.execute('INSERT INTO COASTAL(OCEAN_NAME,FORECAST_TIME,PHENOMENON,WIND_DIRECTION,WIND_POWER,VISIBILITY) VALUES ("%s","%s","%s","%s","%s","%s")' %
+                               (i[0], i[1], i[2], i[3], i[4],i[5]))
+                db.commit()
+            except Exception as e:
+                print(e)
+                db.rollback()
+        db.close()
+    
+    if __name__ == '__main__':
+        url = 'http://www.nmc.cn/publish/marine/newcoastal.html'
+        html_text = get_content(url)
+        result = get_data(html_text)
+        save_db(result)
