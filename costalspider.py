ab4b2ad9dd24b740b1e90922f09ba4d0c66005be
cheunyu <cheunyufoo@gmail.com>
2018-02-02 16:14:44 +0800

    添加pymsql模块 将沿岸海区预报写入数据库

--- costalspider.py
+++ costalspider.py
@@ -1,8 +1,9 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import pymysql
 
 
-class Spider(object):
+class CostalSpider(object):
     """docstring for Spider"""
 
     def __init__(self, ocean_name, forecast_time, phenomenon, wind_direction, wind_power, visibility):
@@ -23,6 +24,11 @@ marineArea = soup.select('td[rowspan=6]')
 # for i in marineArea:
 #     print(i.string)
 tr = soup.select('tr[align=left]')
+db = pymysql.connect("127.0.0.1", "marine_weather",
+                     "qwe123", "marine_weather", charset="utf8")
+cursor = db.cursor()
+cursor.execute("SELECT VERSION()")
+print(cursor.fetchone())
 for i in tr:
     ocean_name = i.attrs['name']
     data_arr = []
@@ -30,9 +36,15 @@ for i in tr:
     for j in range(len(td)):
         if(len(td) == 6):
             if(j != 5):
-                print(td[j + 1])
+                # print(td[j + 1])
                 data_arr.append(td[j + 1])
         else:
             data_arr.append(td[j])
-            # print(td[j])
-    print(len(data_arr))
+    try:
+        cursor.execute('INSERT INTO COASTAL(OCEAN_NAME,FORECAST_TIME,PHENOMENON,WIND_DIRECTION,WIND_POWER,VISIBILITY) VALUES ("%s","%s","%s","%s","%s","%s")' %
+                       (ocean_name, data_arr[0].string, data_arr[1].string, data_arr[2].string, data_arr[3].string, data_arr[4].string))
+        db.commit()
+    except Exception as e:
+        print(e)
+        db.rollback()
+db.close()
